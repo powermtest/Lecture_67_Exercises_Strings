385786d8ea2d780f9cd6f93ea2a3472458eb517b
PowerMtest <powermtest@gmail.com>
2018-02-18 10:35:00 +0100

    Dodane miejsce na nowe cwiczenie, ale nic nie jest zaczete. Chce Win10 zaktualizowac... Lepiej sie zabezpieczyc. ;P

---
+++ Cwiczenie_3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cwiczenie_3
+{
+    //3- Write a program and ask the user to enter a time value in the 24-hour time format(e.g. 19:00).
+    //A valid time should be between 00:00 and 23:59.
+    //If the time is valid, display "Ok"; otherwise, display "Invalid Time".
+    //If the user doesn't provide any values, consider it as invalid time. 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
