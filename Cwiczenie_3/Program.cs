379447c655b94db0972bbd51f2aa5a9743a9cd9c
powermtest <powermtest@gmail.com>
2018-02-19 18:43:51 +0100

    Cwiczenie 3 skonczone z sukcesem. Jedyne co, to mozna przelaczyc je z wersji tesotwej na 'produkcyjna'. ;)

--- Cwiczenie_3/Program.cs
+++ Cwiczenie_3/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Cwiczenie_3
 {
@@ -14,7 +10,67 @@ namespace Cwiczenie_3
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Wprowadz godzine w formacie 'hh:mm'. Zobaczymy, czy ci sie uda. ;)");
+            //var input = Console.ReadLine();
+            var input = string.Empty;
+            //bool godzinyOK = false;
+            //bool separatorOK = false;
+            //bool minutyOK = false;
+            var znaki = new Char[5]; 
+            var error = "Nieprawidłowy format godziny!";
+            var sukces = "Udalo sie wprowadzic godzine w poprawnym formacie!";
+            var testPozytywny1 = "00:00";
+            var testPozytywny2 = "09:59";
+            var testPozytywny3 = "12:44";
+            var testNegatywny1 = "";
+            var testNegatywny2 = "5:11";
+            var testNegatywny3 = "5.33";
+            var testNegatywny4 = "25:00";
+            var testNegatywny5 = ".23:70";
 
+            input = testNegatywny5;
+            if (string.IsNullOrWhiteSpace(input))
+            {
+                Console.WriteLine(error);
+                return;
+            }
+
+            if (input.Length != 5)
+            {
+                Console.WriteLine(error);
+                return;
+            }
+
+            
+            input.CopyTo(0, znaki, 0, 5);
+            var godzina1 = Convert.ToInt16(znaki[0]);
+            //var godzina2 = Convert.ToByte(znaki[1]);
+            var calaGodzina = Convert.ToInt16(input.Substring(0, 2));
+            var minuta1 = Convert.ToInt16(znaki[3]);
+            //var minuta2 = Convert.ToByte(znaki[4]);
+            var minutyCale = Convert.ToInt16(input.Substring(3, 2));
+
+
+
+
+            if (godzina1 >= 0 && godzina1 <= 2 && calaGodzina >= 0 && calaGodzina <=23)
+            {
+                if (znaki[2] == ':')
+                {
+                    if (minuta1 >= 0 && minuta1 <= 5 && minutyCale >= 0 && minutyCale <60)
+                    {
+                        Console.WriteLine(sukces);
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine(error);
+            }
+            
+            
+
+            
         }
     }
 }
